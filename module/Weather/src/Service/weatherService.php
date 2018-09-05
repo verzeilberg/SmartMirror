8b21ff1a5c103265e28a4491d04303f7031f61a9
Sander V <sander@verzeilberg>
2018-09-06 01:02:38 +0200

    Added icons

--- module/Weather/src/Service/weatherService.php
+++ module/Weather/src/Service/weatherService.php
@@ -8,7 +8,7 @@ class weatherService implements weatherServiceInterface {
 
     private $config;
     private $apiKey;
-    
+
     /**
      * Constructor.
      */
@@ -16,6 +16,31 @@ class weatherService implements weatherServiceInterface {
         $this->config = $config;
         $this->apiKey = $config['weatherSettings']['apiKey'];
         $this->units = $config['weatherSettings']['units'];
+        $this->lang = $config['weatherSettings']['language'];
+        $this->country = $config['weatherSettings']['country'];
+        $this->zip = $config['weatherSettings']['zip'];
+        $this->icons = $config['weatherSettings']['icons'];
+    }
+
+    /**
+     *
+     * Get array of weather info
+     *
+     * @return      array
+     *
+     */
+    public function getWeatherInfo($locationType = 'z') {
+
+
+        $weatherData = file_get_contents('http://api.openweathermap.org/data/2.5/weather?zip=' . $this->zip . ',' . $this->country . '&units=' . $this->units . '&lang=' . $this->lang . '&appid=' . $this->apiKey);
+        $weatherData = json_decode($weatherData);
+
+        $data = [];
+        $data['temp'] = round($weatherData->main->temp, 1);
+        $data['icon'] = $this->icons[$weatherData->weather[0]->icon];
+
+
+        return $data;
     }
 
     /**
@@ -25,14 +50,13 @@ class weatherService implements weatherServiceInterface {
      * @return      array
      *
      */
-    public function getWeatherInfo($locationType = 'z') {
-        
-        
-        $weatherData = file_get_contents('http://api.openweathermap.org/data/2.5/weather?zip=1363,nl&units='.$this->units.'&lang=nl&appid='.$this->apiKey);
+    public function getWeatherForecastInfo($locationType = 'z') {
+
+
+        $weatherData = file_get_contents('http://api.openweathermap.org/data/2.5/forecast?zip=' . $this->zip . ',' . $this->country . '&units=' . $this->units . '&lang=' . $this->lang . '&appid=' . $this->apiKey);
         $weatherData = json_decode($weatherData);
-        
+
         return $weatherData;
-        
     }
 
 }
